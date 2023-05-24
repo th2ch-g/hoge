4306ba8f3e87bb668108896e52aedffa1288b34c
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2023-05-24 16:48:42 +0900

    add

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 // next.config.js
 
-import mdxconfig from '@next/mdx';
-import remarkgfm from 'remark-gfm';
-import rehypeAttrs from 'rehype-attr';
-
-const withMDX = mdxconfig({
-    extension: /\.mdx?$/,
-    options: {
-        // If you use remark-gfm, you'll need to use next.config.mjs
-        // as the package is ESM only
-        // https://github.com/remarkjs/remark-gfm#install
-        remarkPlugins: [remarkgfm],
-        rehypePlugins: [() => rehypeAttrs({ properties: 'attr' })],
-        // If you use `MDXProvider`, uncomment the following line.
-        providerImportSource: '@mdx-js/react',
-    },
-});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,5 +9,4 @@ const nextConfig = {
     reactStrictMode: true,
 };
 
-// Merge MDX config with Next.js config
-export default withMDX(nextConfig);
+export default nextConfig;
