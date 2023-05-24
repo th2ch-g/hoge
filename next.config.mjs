9d03787f1d654373a4fc31c40be328ae9e1b8ae3
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2023-05-24 18:11:58 +0900

    add

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,9 @@ const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     // Optionally, add any other Next.js config below
     reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    }
 };
 
 export default nextConfig;
