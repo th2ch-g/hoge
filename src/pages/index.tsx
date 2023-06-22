d63e551ecb3360e5438c28af3128740e9382123e
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2023-06-22 16:12:44 +0900

    fix: yaml

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,7 +101,7 @@ export default function Home() {
                             href=""
                             target="_blank"
                         >
-                            <Icon 
+                            <Icon
                                 as={AiFillLinkedin}
                                 boxSize={50}
                             />
