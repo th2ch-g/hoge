92612b1f7a705cf660f20f3c42b00f18dad485bd
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-03-15 23:09:13 +0900

    impl works

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,3 +1,4 @@
+pub mod card;
 pub mod footer;
 pub mod layout;
 pub mod navbar;
--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,4 +1,3 @@
-use crate::components::footer::Footer;
 use crate::components::layout::Layout;
 use crate::components::navbar::Navbar;
 use crate::icons::gist::IconGist;
@@ -27,22 +26,36 @@ impl Component for IndexPage {
               <Navbar />
                 // https://tailwindcomponents.com/component/list-blog-post-tailwindcss
                 <Layout>
-                      <h2 class="font-bold text-center text-6xl text-slate-700 font-display">
-                      {"th2ch-g"}
-                      </h2>
-                      <p class="text-center mt-4 font-medium text-slate-500">{"homepage built by yew"}</p>
-                      <div class="flex gap-6 mt-20 justify-center">
-                        <IconHome />
-                        <IconGithub />
-                        <IconGist />
-                        <IconKaggle />
-                        <IconLinkedin />
-                      </div>
-                      <div class="flex gap-6 mt-20 justify-center">
-                        <img src="img/daiowasabi1.jpg" width=400 height=200 />
-                      </div>
+                  <h2 class="font-bold text-center text-6xl text-slate-700 font-display">
+                  {"th2ch-g"}
+                  </h2>
+                  <p class="text-center mt-4 font-medium text-slate-500">{"homepage built by yew"}</p>
+
+                  <div class="flex gap-6 mt-20 justify-center">
+                    <IconHome />
+                    <IconGithub />
+                    <IconGist />
+                    <IconKaggle />
+                    <IconLinkedin />
+                  </div>
+
+                  // todo!("component")
+                  <div class="bg-slate-100 justify-center p-4 text-sm font-semibold backdrop-blur-sm">
+                      // <p class="text-center text-slate-500">{"LANG"}</p>
+                      <ul
+                          class="flex mt-4 flex-wrap items-center justify-center justify-start gap-2 gap-y-3 [&>li]:border-2 [&>li]:border-[#2f2a47] [&>li]:px-3 [&>li]:py-1 [&>li]:rounded-[4px] [&>li]:transition-all [&>li]:duration-150 [&>li]:ease-in [&>li:hover]:scale-105 [&>li:hover]:cursor-pointer">
+                          <li>{"Rust"}</li>
+                          <li>{"Python"}</li>
+                          <li>{"Bioinformatics"}</li>
+                      </ul>
+                  </div>
+
+                  <div class="flex gap-6 mt-20 justify-center">
+                    <img src="img/daiowasabi1.jpg" width=400 height=200 />
+                  </div>
                 </Layout>
               // <Footer />
+
             </div>
         }
     }
