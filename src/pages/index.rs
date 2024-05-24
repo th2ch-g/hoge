8f12176bebe98a3e493c90b5bc27210b468167a0
th2ch-g <76892059+th2ch-g@users.noreply.github.com>
2024-05-25 02:24:59 +0900

    add

--- src/pages/index.rs
+++ src/pages/index.rs
@@ -1,5 +1,4 @@
 use crate::components::footer::Footer;
-use crate::components::icons::gist::IconGist;
 use crate::components::icons::github::IconGithub;
 use crate::components::icons::home::IconHome;
 use crate::components::icons::kaggle::IconKaggle;
@@ -33,21 +32,20 @@ impl Component for IndexPage {
                   <div class="flex gap-6 mt-20 justify-center">
                     <IconHome />
                     <IconGithub />
-                    <IconGist />
                     <IconKaggle />
                     <IconLinkedin />
                   </div>
 
-                  // todo!("component")
-                  <div class="bg-slate-100 justify-center p-4 text-sm font-semibold backdrop-blur-sm">
-                      // <p class="text-center text-slate-500">{"LANG"}</p>
-                      <ul
-                          class="flex mt-4 flex-wrap items-center justify-center justify-start gap-2 gap-y-3 [&>li]:border-2 [&>li]:border-[#2f2a47] [&>li]:px-3 [&>li]:py-1 [&>li]:rounded-[4px] [&>li]:transition-all [&>li]:duration-150 [&>li]:ease-in [&>li:hover]:scale-105 [&>li:hover]:cursor-pointer">
-                          <li>{"Rust"}</li>
-                          <li>{"Python"}</li>
-                          <li>{"Bioinformatics"}</li>
-                      </ul>
-                  </div>
+                  // // todo!("component")
+                  // <div class="bg-slate-100 justify-center p-4 text-sm font-semibold backdrop-blur-sm">
+                  //     // <p class="text-center text-slate-500">{"LANG"}</p>
+                  //     <ul
+                  //         class="flex mt-4 flex-wrap items-center justify-center justify-start gap-2 gap-y-3 [&>li]:border-2 [&>li]:border-[#2f2a47] [&>li]:px-3 [&>li]:py-1 [&>li]:rounded-[4px] [&>li]:transition-all [&>li]:duration-150 [&>li]:ease-in [&>li:hover]:scale-105 [&>li:hover]:cursor-pointer">
+                  //         <li>{"Rust"}</li>
+                  //         <li>{"Python"}</li>
+                  //         <li>{"Bioinformatics"}</li>
+                  //     </ul>
+                  // </div>
 
                   <div class="flex gap-6 mt-20 justify-center">
                     <img src="https://raw.githubusercontent.com/th2ch-g/home/main/img/daiowasabi1.jpg" width=400 height=200 />
@@ -55,16 +53,22 @@ impl Component for IndexPage {
 
 
                   // comment
-                  <Title title={""} subtitle={"Comments to this content via your github account"} />
-                  <script
-                     src="https://utteranc.es/client.js"
-                     repo="th2ch-g/home"
-                     issue-term="pathname"
-                     theme="github-light"
-                     crossorigin="anonymous"
-                     async=true
-                   ></script>
-
+                    <div class="flex mt-20 justify-center w-full">
+                      <details class="w-full max-w-3xl">
+                        <summary class="text-center cursor-pointer mt-10">{"Open comment"}</summary>
+                        <div class="p-4 bg-white border rounded-md">
+                          <Title title={""} subtitle={"Comments to this content via your github account"} />
+                          <script
+                            src="https://utteranc.es/client.js"
+                            repo="th2ch-g/home"
+                            issue-term="pathname"
+                            theme="github-light"
+                            crossorigin="anonymous"
+                            async=true
+                          ></script>
+                        </div>
+                      </details>
+                    </div>
                 </Layout>
               <Footer />
 
